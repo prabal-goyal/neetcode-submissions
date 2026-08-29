f20c5cdd07248dd335f2f3af4923fbf5275ef520
prabal-goyal <89393467+prabal-goyal@users.noreply.github.com>
2026-08-29 10:22:46 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const map = new Map();
+
+        for(const str of strs) {
+            const key = [...str].sort().join();
+
+            if(!map.has(key)) {
+                map.set(key, []);
+            }
+
+            map.get(key).push(str);
+        }
+
+        return [...map.values()]
+    }
+}
